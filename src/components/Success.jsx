91053bad3be0f204cd3bfe77f1fda3dfd7dc0b4e
Marcos Gramajo <marcosgramajo96@gmail.com>
2024-03-06 00:26:31 -0300

    change credentials

--- src/components/Success.jsx
+++ src/components/Success.jsx
@@ -1,40 +1,42 @@
 import React, {useEffect, useState } from 'react'
 import { Context } from "./ContextProvider";
 import axios from 'axios'
+import { useParams } from 'react-router-dom';
 
 const Success = () => {
 
-    const [participant, setParticipant] = useState(JSON.parse(window.localStorage.getItem('participant')))
-    const [orderData, setOderData] = useState(JSON.parse(window.localStorage.getItem('orderData')))
-    const [resData, setResData] = useState('')
-    const getResponse = async (  ) => {
-        try {
-            const response = await axios.get(`https://backghtucuman.netlify.app/.netlify/functions/server/feedback?status=approved&participant_id=${participant.id}&quantity_votes=${orderData.quantity}`);
-            setResData(response.data)
-        } catch (error) {
-            console.error('Error fetching data:', error);
-        }
-    };
+  //const { id } = useParams();
 
-    useEffect(() => {
-        getResponse()
-    }, [])
+  const [participant, setParticipant] = useState(JSON.parse(window.localStorage.getItem('participant')))
+  const [orderData, setOderData] = useState(JSON.parse(window.localStorage.getItem('orderData')))
+  const [resData, setResData] = useState('')
 
-    useEffect(() => {
-        console.log(resData)
-        console.log(orderData)
-    }, [resData])
+  const queryString = window.location.search;
+
+  const urlParams = new URLSearchParams(queryString);
+  const payment_id = urlParams.get('payment_id');
 
 
-
-    console.log(participant)
-    return (
-      <div>
-        <img src={participant.photo} alt="participant"/>
-        {participant.name}
-      </div>
-    );
+  const getResponse = async (  ) => {
+      try {
+          const response = await axios.get(`https://backghtucuman.netlify.app/.netlify/functions/server/feedback?status=approved&participant_id=${participant.id}&quantity_votes=${orderData.quantity}&payment_id=${payment_id}`);
+          setResData(response.data)
+      } catch (error) {
+          console.error('Error fetching data:', error);
+      }
   };
+
+  useEffect(() => {
+      getResponse()
+  }, [])
+
+  return (
+    <div>
+      <img src={participant.photo} alt="participant"/>
+      {participant.name}
+    </div>
+  );
+};
 
 
 export default Success;
